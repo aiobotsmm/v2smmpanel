5683908d971ab86d7e0658612d1c0267e7fc4341
aiobotsmm <snappiiuser09@gmail.com>
2025-06-28 20:18:37 +0530

    Update admin_utils.py

--- admin_utils.py
+++ admin_utils.py
@@ -4,4 +4,4 @@ from db import cur
 def is_admin(user_id: int) -> bool:
     row = cur.execute("SELECT 1 FROM admins WHERE user_id = ?", (user_id,)).fetchone()
     return bool(row)
-    return user_id in ADMIN_IDS
+   # return user_id in ADMIN_IDS
