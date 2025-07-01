e19b154e641746665454538311b79eb34f1993b5
aiobotsmm <snappiiuser09@gmail.com>
2025-07-01 18:48:38 +0530

    Update db.py

--- db.py
+++ db.py
@@ -39,7 +39,7 @@ def initialize_database():
         amount REAL,
         txn_id TEXT UNIQUE,
         status TEXT DEFAULT 'pending'
-        created_at TIMESTAMP DEFAULT (datetime('now', 'utc'))
+        created_at TEXT DEFAULT (datetime('now'))
     );
 
     CREATE TABLE IF NOT EXISTS complaint_tokens (
